b15f372721b1c891128e2842170109ded5cb2f09
e1b20106 <e1b20106@st.oit.ac.jp>
2022-10-09 21:51:04 +0900

    CPUの手をランダムに変更

--- src/main/java/oit/is/z1102/kaizi/janken/contoroller/JankenController.java
+++ src/main/java/oit/is/z1102/kaizi/janken/contoroller/JankenController.java
@@ -6,15 +6,63 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import oit.is.z1102.kaizi.janken.model.Janken;
 
 @Controller
-@RequestMapping("/janken")
 public class JankenController {
 
-  @PostMapping
-  public String janken(@RequestParam String name, ModelMap model) {
+  @PostMapping("/janken")
+  public String name(@RequestParam String name, ModelMap model) {
     model.addAttribute("name", name);
     return "janken.html";
   }
+
+  /**
+   * @return
+   */
+
+  @GetMapping("/janken/{te}")
+  public String janken(@PathVariable("te") String te, ModelMap model) {
+
+    int user = 0;
+    int cpu = 0;
+    Janken janken = new Janken();
+
+    if (te.equals("Gu")) {
+      user = 0;
+    } else if (te.equals("Ty")) {
+      user = 1;
+    } else if (te.equals("Pa")) {
+      user = 2;
+    }
+
+    janken.setUser(user);
+    cpu = janken.getCpu();
+    janken.result();
+
+    String result = janken.getResult();
+
+    if (user == 0) {
+      model.addAttribute("tuser", "Gu");
+    } else if (user == 1) {
+      model.addAttribute("tuser", "Ty");
+    } else {
+      model.addAttribute("tuser", "Pa");
+    }
+
+    if (cpu == 0) {
+      model.addAttribute("tcpu", "Gu");
+    } else if (cpu == 1) {
+      model.addAttribute("tcpu", "Ty");
+    } else {
+      model.addAttribute("tcpu", "Pa");
+    }
+
+    model.addAttribute("result", result);
+
+    return "janken.html";
+
+  }
+
 }
