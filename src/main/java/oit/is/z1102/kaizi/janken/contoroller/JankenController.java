94fa51044a1ef6b5d3f277d27cbda7b9e93d35eb
e1b20106 <e1b20106@st.oit.ac.jp>
2022-10-16 23:40:44 +0900

    オブジェクトの郷愁を実装

--- src/main/java/oit/is/z1102/kaizi/janken/contoroller/JankenController.java
+++ src/main/java/oit/is/z1102/kaizi/janken/contoroller/JankenController.java
@@ -2,26 +2,34 @@ package oit.is.z1102.kaizi.janken.contoroller;
 
 import java.security.Principal;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
 
 import oit.is.z1102.kaizi.janken.model.Janken;
+import oit.is.z1102.kaizi.janken.model.Entry;
 
 import java.util.Random;
 
 @Controller
 public class JankenController {
 
+  @Autowired
+  private Entry room;
+
   @GetMapping("/janken")
-  public String name(ModelMap model, Principal prin) {
+  public String name(ModelMap model1, ModelMap model2, Principal prin) {
     String loginUser = prin.getName(); // ログインユーザ情報
-    model.addAttribute("name", loginUser);
+    this.room.addUser(loginUser);
+    model1.addAttribute("room", this.room);
+    model2.addAttribute("login_user", loginUser);
     return "janken.html";
   }
+
 
   /*
    * @PostMapping("/janken")
